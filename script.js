c45205efa2b4bf2e3fa03462ad712170e8641464
Luis C. Beltran <lcarlosbeltran@gmail.com>
2023-01-31 10:15:50 -0400

    Search function working

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const table = [
     {
         Institution: 'Bee Me Kidz',
         Category: 'Education, Food Support',
-        Location: 'Saint John – North End - Centennial School - 10 Constance Street Saint John, New Brunswick E2K 2P8 / Saint John – East End - Glen Falls School - 10 Princess Court - Saint John, New Brunswick E2H 1X9 / Charlotte County – Milltown Elementary School - 434 Milltown Blvd St. Stephen, New Brunswick E3L 1J8',
-        Website: 'http://beemekidz.com',
+        Location: 'Saint John <br> North End - Centennial School - 10 Constance Street - E2K 2P8 <br> <br> Saint John <br> East End - Glen Falls School - 10 Princess Court - E2H 1X9 <br><br> Charlotte County – Milltown Elementary School - 434 Milltown Blvd St. Stephen, New Brunswick E3L 1J8',
+        Website: '<a href="http://beemekidz.com" target="_blank">http://beemekidz.com</a>',
         Phone: 'Phone: 5063497331',
         Notes: 'Educational program on saturdays. It includes a nutritious brunch and snacks. There is also a parent education component with free childcare for the siblings. Registration must be done in advance by phone. It operates throughout the school year.',
         Free: 'Yes',
@@ -11,9 +11,9 @@ const table = [
     {
         Institution: 'bgc Greater Saint John',
         Category: 'Education, After School',
-        Location: 'Main Club: 1 Paul Harris Street - P.O. Box 2441 - Saint John, NB E2L 3V9 / Seaside Park Elementary School - 627 Havelock St, Saint John, NB E2M 2V4 / St. John the Baptist, King Edward School - 223 St James St, Saint John, NB E2L 1W3 / St. John the Baptist/King Edward School - 223 St James St, Saint John, NB E2L 1W3 / Belleisle Elementary School - 1775 ROUTE, Springfield, NB E5T 2K1',
-        Website: 'http://sjbgclub.com',
-        Phone: 'Phone: (506) 634-2011 - Fax: (506) 648-0804 - Main Office: 634-2011 ext.221',
+        Location: 'Main Club: 1 Paul Harris Street - P.O. Box 2441 - Saint John, NB E2L 3V9 <br><br> Seaside Park Elementary School - 627 Havelock St, Saint John, NB E2M 2V4 <br><br> St. John the Baptist, King Edward School - 223 St James St, Saint John, NB E2L 1W3 <br><br> St. John the Baptist / King Edward School - 223 St James St, Saint John, NB E2L 1W3 <br><br> Belleisle Elementary School - 1775 ROUTE, Springfield, NB E5T 2K1',
+        Website: '<a href="http://sjbgclub.com" target="_blank">http://sjbgclub.com</a>',
+        Phone: 'Phone: (506) 634-2011 <br> Fax: (506) 648-0804 <br> Main Office: 634-2011 ext.221',
         Notes: 'Afterschool programs cost aproximately CAD $90/week. They also offer summer camps and early morning programs. There are some youth programs that are free.',
         Free: 'No',
     },
@@ -21,7 +21,7 @@ const table = [
         Institution: 'The Teen Resource Centre',
         Category: 'Education',
         Location: '28 Richmond Street, Saint John, NB E2L 3B2',
-        Website: 'https://www.trc4youth.ca',
+        Website: '<a href="https://www.trc4youth.ca" target="_blank">https://www.trc4youth.ca</a>',
         Phone: 'Phone: 506-638-2372',
         Notes: 'TRC offers tutoring and mentoring programs for youth between the ages of 12 and 24.',
         Free: 'Yes',
@@ -33,7 +33,7 @@ const search = document.querySelector('.filter-input');
 const output = document.querySelector('.output');
 
 window.addEventListener('DOMContentLoaded', loadTable);
-// search.addEventListener('input', filter);
+search.addEventListener('input', filter);
 
 function loadTable () {
     let temp = `<table> <tr>`;
@@ -56,4 +56,45 @@ function loadTable () {
     temp+=`</table>`;
 
     output.innerHTML = temp;
+}
+
+function filter (e) {
+    let results;
+    let temp = "";
+
+    results = table.filter(item => 
+        item.Institution.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.Category.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.Location.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.Website.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.Phone.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.Notes.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        item.Free.toLowerCase().includes(e.target.value.toLowerCase())
+        );
+
+        if (results.length > 0) {
+            temp = `<table> <tr>`;
+            tableHeader.forEach(header=> temp+= `<th> ${header.toUpperCase()} </th>`);
+            temp+= `<tr>`;
+            results.forEach(row => {
+                temp +=`
+                <tr>
+                    <td>${row.Institution}</td>
+                    <td>${row.Category}</td>
+                    <td>${row.Location}</td>
+                    <td>${row.Website}</td>
+                    <td>${row.Phone}</td>
+                    <td>${row.Notes}</td>
+                    <td>${row.Free}</td>
+                </tr>
+                `
+            });
+
+            temp+=`</table>`;
+        } else {
+            temp = `<div class="no-item">Item not found</div>`
+        }
+
+        output.innerHTML = temp;
+
 }
